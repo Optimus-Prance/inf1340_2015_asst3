7583bf9f22c1ebded691d539e1c4b0a672105ef3
Darius Chow <dariuschow@gmail.com>
2015-12-03 11:38:44 -0500

    exercise1.py: Separated test cases into separate functions for clarity.

--- test_exercise1.py
+++ test_exercise1.py
@@ -64,41 +64,48 @@ def filter_employees(row):
 # TEST FUNCTIONS ##
 ###################
 
-def test_selection():
+def test_selection_some_results():
     """
-    Test select operation.
+    Test select operation such that some rows get selected, some don't get selected
     """
-    #Some gets selected, some don't get selected
     result = [["Surname", "FirstName", "Age", "Salary"],
               ["Verdi", "Nico", 36, 4500],
               ["Smith", "Mark", 40, 3900]]
 
     assert is_equal(result, selection(EMPLOYEES, filter_employees))
 
-    #Selection returns everything
+def test_selection_all_results():
+    """
+    Test select operation with selection that returns everything
+    """
     E_1 = [["Surname", "FirstName", "Age", "Salary"],
            ["Verdi", "Nico", 36, 4500],
            ["Smith", "Mark", 40, 3900]]
 
     assert is_equal(E_1, selection(E_1, filter_employees))
 
-    #Selection returns nothing
+def test_selection_no_results():
+    """
+    Test select operation where nothing gets selected.
+    """
     E_2 = [["Surname", "FirstName", "Age", "Salary"],
            ["Smith", "Mary", 25, 2000],
            ["Black", "Lucy", 40, 3000]]
 
     assert is_equal(None, selection(E_2, filter_employees))
 
-    #Selection on empty table
+def test_selection_empty_input_table():
+    """
+    Test select operation where input table is an empty table.
+    """
     E_3 = [["Surname", "FirstName", "Age", "Salary"]]
 
     assert is_equal(None, selection(E_3, filter_employees))
 
-def test_projection():
+def test_projection_some_attributes():
     """
-    Test projection operation.
+    Test projection operation such that some but not all attributes get projected.
     """
-    # Project some attributes
     result = [["Surname", "FirstName"],
               ["Smith", "Mary"],
               ["Black", "Lucy"],
@@ -107,31 +114,42 @@ def test_projection():
 
     assert is_equal(result, projection(EMPLOYEES, ["Surname", "FirstName"]))
 
-    # Project all attributes
+def test_projection_all_attributes():
+    """
+    Test projection operation with all attributes projected.
+    """
     attribute_list = ["Surname", "FirstName", "Age", "Salary"]
     empty_table = [attribute_list]
     assert is_equal(result, projection(EMPLOYEES, attribute_list))
     assert is_equal(None,projection(empty_table,attribute_list))
 
-    # Project No attributes
+def test_projection_no_attributes():
+    """
+    Test projection operation with no attributes getting projected.
+    """
     assert is_equal(None, projection(EMPLOYEES, []))
     assert is_equal(None, projection(empty_table, []))
 
-    # Attribute not found (all mismatch, and partial mismatch)
+def test_projection_attribute_not_found():
+    """
+    Test projection operation if attribute not found.
+    """
+    # All mismatch
     try:
         projection(EMPLOYEES, ["LastName", "GivenName"])
         assert False
     except:
         assert True
+    # Partial mismatch
     try:
         projection(EMPLOYEES, ["Date of Birth", "Income"])
         assert False
     except:
         assert True
 
-def test_cross_product():
+def test_cross_product_basic():
     """
-    Test cross product operation.
+    Test cross product operation with basic/generic inputs.
     """
 
     result_1 = [["Employee", "Department", "Department", "Head"],
@@ -154,7 +172,7 @@ def test_cross_product():
 
     assert is_equal(result_2, cross_product(R2, R1))
 
-    # Cross product using larger tables
+    # Using larger tables
     result_3 = [["Surname", "FirstName", "Age", "Salary", "Employee", "Department"],
                 ["Smith", "Mary", 25, 2000, "Smith", "sales"],
                 ["Smith", "Mary", 25, 2000, "Black", "production"],
@@ -171,40 +189,49 @@ def test_cross_product():
 
     assert is_equal(result_3, cross_product(EMPLOYEES, R1))
 
-    # Cross product of same tables
-    result_4 = [["Employee", "Department", "Employee", "Department"],
+def test_cross_product_same_tables():
+    """
+    Test cross product operation using the same tables.
+    """
+    result = [["Employee", "Department", "Employee", "Department"],
                 ["Smith", "sales", "Smith", "sales"],
                 ["Black", "production", "Black", "production"],
                 ["White", "production", "White", "production"]]
 
-    assert is_equal(result_4, cross_product(R1, R1))
+    assert is_equal(result, cross_product(R1, R1))
 
-    # Cross product using table with one row
+def test_cross_product_one_row_table():
+    """
+    Test cross product operation using the a table with only one row.
+    """
     table_with_one_row = [["ID Number", "Date of Birth"],
                           [0001, "01/01/2001"]]
 
-    result_5 = [["ID Number", "Date of Birth", "Surname", "FirstName", "Age", "Salary"],
+    result_1 = [["ID Number", "Date of Birth", "Surname", "FirstName", "Age", "Salary"],
                 [0001, "01/01/2001", "Smith", "Mary", 25, 2000],
                 [0001, "01/01/2001", "Black", "Lucy", 40, 3000],
                 [0001, "01/01/2001", "Verdi", "Nico", 36, 4500],
                 [0001, "01/01/2001", "Smith", "Mark", 40, 3900]]
 
-    assert is_equal(result_5, cross_product(table_with_one_row, EMPLOYEES))
+    assert is_equal(result_1, cross_product(table_with_one_row, EMPLOYEES))
 
-    result_6 = [["Surname", "FirstName", "Age", "Salary", "ID Number", "Date of Birth"],
+    result_2 = [["Surname", "FirstName", "Age", "Salary", "ID Number", "Date of Birth"],
                 ["Smith", "Mary", 25, 2000, 0001, "01/01/2001"],
                 ["Black", "Lucy", 40, 3000, 0001, "01/01/2001"],
                 ["Verdi", "Nico", 36, 4500, 0001, "01/01/2001"],
                 ["Smith", "Mark", 40, 3900, 0001, "01/01/2001"]]
 
-    assert is_equal(result_6, cross_product(EMPLOYEES, table_with_one_row))
+    assert is_equal(result_2, cross_product(EMPLOYEES, table_with_one_row))
 
-    result_7 = [["ID Number", "Date of Birth", "ID Number", "Date of Birth"],
+    result_3 = [["ID Number", "Date of Birth", "ID Number", "Date of Birth"],
                 [0001, "01/01/2001", 0001, "01/01/2001"]]
 
-    assert is_equal(result_7, cross_product(table_with_one_row, table_with_one_row))
+    assert is_equal(result_3, cross_product(table_with_one_row, table_with_one_row))
 
-    # Cross product using empty table
+def test_cross_product_empty_table():
+    """
+    Test cross product operation using an empty table.
+    """
     empty_table = [["ID Number", "Date of Birth"]]
     assert is_equal(None, cross_product(empty_table, R1))
     assert is_equal(None, cross_product(empty_table, R2))
