4f82f8f2268abff8b232932fe20548e41ffa580c
Darius Chow <dariuschow@gmail.com>
2015-12-09 01:04:39 -0500

    added test_returning_citizen_no_citizens.json; test_exercise2.py: added test case for no citizens

--- test_exercise2.py
+++ test_exercise2.py
@@ -25,3 +25,8 @@ def test_returning():
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Accept", "Accept", "Quarantine"]
 
+def test_returning():
+    """
+    No travellers are returning.
+    """
+    assert decide("test_returning_citizen_no_citizens.json") == []
