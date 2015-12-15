87ae6b8a730f9939d450a5e7b44c4344fc58c8cd
Darius Chow <dariuschow@gmail.com>
2015-12-14 23:11:51 -0500

    changed json files; test_exercise2.py: changed/added test cases for files

--- test_exercise2.py
+++ test_exercise2.py
@@ -36,6 +36,10 @@ REQUIRED_FIELDS = ("first_name", "last_name", "birth_date", "passport", "home", 
 LOCATION_FIELDS = ("home", "from", "via")
 REQUIRED_FIELDS_LOCATION = ("city", "region", "country")
 REASON_FOR_ENTRY = ('returning', 'visiting')
+'''
+DATE_TODAY is a string of the format YYYY-MM-DD representing today's date.
+'''
+DATE_TODAY = "2015-12-16"
 
 def test_returning():
     """
@@ -77,7 +81,7 @@ def test_decide_missing_required_information_file():
                     if not valid_location_field(person[item]):
                         required_fields_included = False
                 elif item is "passport":
-                    assert valid_passport_or_visa(person[item])
+                    assert valid_passport(person[item])
         assert required_fields_included == False
 
 def test_decide_missing_required_information():
@@ -112,6 +116,10 @@ def test_decide_unknown_locations():
     assert decide("test_decide_unknown_locations.json", COUNTRIES_FILE) == ["Reject"] * 5
 
 def test_decide_KAN_citizens_file():
+    """
+    Ensure that the file contains travellers that have are KAN citizens and from/via locations that do not have
+    medical advisories.
+    """
     with open("test_decide_KAN_citizens.json", "r") as citizen_file:
         citizen_content = citizen_file.read()
     citizen_json = json.loads(citizen_content)
@@ -119,6 +127,8 @@ def test_decide_KAN_citizens_file():
     assert valid_file_contents(citizen_json)
     for person in citizen_json:
         for item in person:
+            if item == "home":
+                assert person["home"]["country"] == "KAN"
             location_fields_to_check = LOCATION_FIELDS[1:]  # Excludes home location field of country_code KAN
             if item in location_fields_to_check:
                 country_code = person[item]['country']
@@ -131,13 +141,60 @@ def test_decide_KAN_citizens():
     """
     assert decide("test_decide_KAN_citizens.json", COUNTRIES_FILE) == ["Accept"] * 3
 
+def test_decide_visitors_require_visas_valid_visas_file():
+    """
+    Ensure that the file contains travellers that have are visitors, from locations that require visas, and they
+    have valid visas.
+    """
+    with open("test_decide_visitors_require_visas_valid_visas.json", "r") as citizen_file:
+        citizen_content = citizen_file.read()
+    citizen_json = json.loads(citizen_content)
+
+    assert valid_file_contents(citizen_json)
+    for person in citizen_json:
+        country_code = person["home"]["country"]
+        assert COUNTRIES[country_code]['visitor_visa_required'] == "1"
+        valid = False
+        if "visa" in person:
+            if valid_visa(person["visa"],DATE_TODAY):
+                valid = True
+        for item in LOCATION_FIELDS:
+            if item in person:
+                country_code_check_medical_advisory = person[item]['country']
+                assert COUNTRIES[country_code_check_medical_advisory]['medical_advisory'] == ""
+        assert valid
+
 def test_decide_visitors_require_visas_valid_visas():
     """
     Visitors that have their home location other than KAN (but still a valid location), and because of their home
     countries, they require a visa to enter. Their visas are valid. These travellers did not travel from or through
     a country with a medical advisory and all required information is present.
     """
-    assert decide("test_decide_visitors_require_visas_valid_visas.json", COUNTRIES_FILE) == ["Accept"] * 1
+    assert decide("test_decide_visitors_require_visas_valid_visas.json", COUNTRIES_FILE) == ["Accept"] * 4
+
+def test_decide_visitors_require_visas_invalid_visas_file():
+    """
+    Ensure that the file contains travellers that are visitors, from locations that require visas, and they
+    do not have valid visas.
+    """
+    with open("test_decide_visitors_require_visas_invalid_visas.json", "r") as citizen_file:
+        citizen_content = citizen_file.read()
+    citizen_json = json.loads(citizen_content)
+
+    assert valid_file_contents(citizen_json)
+    for person in citizen_json:
+        country_code = person["home"]["country"]
+        assert COUNTRIES[country_code]['visitor_visa_required'] == "1"
+        invalid_visa = True
+        if "visa" in person:
+            if valid_visa(person["visa"],DATE_TODAY):
+                invalid_visa = False
+        for item in LOCATION_FIELDS:
+            if item in person:
+                country_code_check_medical_advisory = person[item]['country']
+                assert COUNTRIES[country_code_check_medical_advisory]['medical_advisory'] == ""
+        assert invalid_visa
+
 
 def test_decide_visitors_require_visas_invalid_visas():
     """
@@ -146,7 +203,24 @@ def test_decide_visitors_require_visas_invalid_visas():
     These travellers did not travel from or through a country with a medical advisory and all required information
     is present.
     """
-    assert decide("test_decide_visitors_require_visas_invalid_visas.json", COUNTRIES_FILE) == ["Accept"] * 1
+    assert decide("test_decide_visitors_require_visas_invalid_visas.json", COUNTRIES_FILE) == ["Reject"] * 4
+
+def test_decide_visitors_visas_not_needed_file():
+    """
+    Ensure that the file contains travellers that are visitors, from locations that do not require visas.
+    """
+    with open("test_decide_visitors_visas_not_needed.json", "r") as citizen_file:
+        citizen_content = citizen_file.read()
+    citizen_json = json.loads(citizen_content)
+
+    assert valid_file_contents(citizen_json)
+    for person in citizen_json:
+        country_code = person["home"]["country"]
+        assert COUNTRIES[country_code]['visitor_visa_required'] == "0"
+        for item in LOCATION_FIELDS:
+            if item in person:
+                country_code_check_medical_advisory = person[item]['country']
+                assert COUNTRIES[country_code_check_medical_advisory]['medical_advisory'] == ""
 
 def test_decide_visitors_visas_not_needed():
     """
@@ -156,13 +230,31 @@ def test_decide_visitors_visas_not_needed():
     """
     assert decide("test_decide_visitors_visas_not_needed.json", COUNTRIES_FILE) == ["Accept"] * 1
 
+def test_decide_KAN_citizens_via_country_with_medical_advisory_file():
+    with open("test_decide_KAN_citizens_via_country_with_medical_advisory.json", "r") as citizen_file:
+        citizen_content = citizen_file.read()
+    citizen_json = json.loads(citizen_content)
+
+    assert valid_file_contents(citizen_json)
+    for person in citizen_json:
+        traveled_via_medical_advisory_country = False
+        for item in person:
+            if item == "home":
+                assert person["home"]["country"] == "KAN"
+            location_fields_to_check = LOCATION_FIELDS[1:]  # Excludes home location field of country_code KAN
+            if item in location_fields_to_check:
+                country_code = person[item]['country']
+                if COUNTRIES[country_code]['medical_advisory'] != "":
+                    traveled_via_medical_advisory_country = True
+        assert traveled_via_medical_advisory_country
+
 def test_decide_KAN_citizens_via_country_with_medical_advisory():
     """
     Testing for KAN citizens that travelled from or via a country with a medical advisory. All required information
     is present.
     """
     assert decide("test_decide_KAN_citizens_via_country_with_medical_advisory.json", COUNTRIES_FILE) ==\
-           ["Quarantine"] * 1
+           ["Quarantine"] * 4
 
 def test_decide_visitors_via_country_with_medical_advisory():
     """
@@ -241,30 +333,103 @@ def test_valid_country_code():
     assert valid_country_code("ALB",countries)
     assert valid_country_code("MLC",countries) == False
 
-def valid_passport_or_visa(passport_or_visa_number):
+def valid_visa(visa, date_today):
     """
-    This function checks to see if a passport or visa number is valid, as defined by having five groups of
+    This function checks to see if a visa is valid, as defined by having a visa number of five groups of
+    alphanumeric characters (case-insensitive), separated by dashes, and a date within the past two years.
+
+    :param visa: a dictionary with "code" and "date" that represents a visa number and date respectively
+    :param date_today: string that represents a passport or visa number
+    :return: True, if the string conforms to the regular expression, False otherwise.
+    """
+
+    valid_regex = re.compile(r'^([\dA-Za-z]{5}-){4}[\dA-Za-z]{5}$')
+    valid_regex_match = valid_regex.search(visa["code"])
+    valid_code = True
+    if valid_regex_match is None:
+        valid_code = False
+    valid_date = date_within_two_years(visa["date"], date_today)
+    return valid_code and valid_date
+
+
+def test_valid_visa():
+    d1 = {"date": "2015-02-24", "code": "6P294-42HR2-95PSF-93NFF-2TEWF"}
+    assert valid_visa(d1, "2015-12-16")
+    d2 = {"date": "2013-12-25", "code": "TJq2R-25stx-Fyc52-02rm0-420DS"}
+    assert valid_visa(d2, "2015-12-16")
+    d3 = {"date": "2015-02-24", "code": "33T0R-8T3T2-W_C77-243GE-42O_D"}
+    assert valid_visa(d3, "2015-12-16") == False
+    d4 = {"date": "2015-02-24", "code": "T2EW5-WT255-019RW-2RWS4-42FFX-TNX2R"}
+    assert valid_visa(d4, "2015-12-16") == False
+    d5 = {"date": "2013-12-17", "code": "TJq2R-25stx-Fyc52-02rm0-420DS"}
+    assert valid_visa(d5, "2015-12-16")
+    d6 = {"date": "2013-12-01", "code": "TJq2R-25stx-Fyc52-02rm0-420DS"}
+    assert valid_visa(d6, "2015-12-16") == False
+
+def date_within_two_years(date_to_test, date_today):
+    """
+    Determine if the date_to_test is within two years of the date_today. Assumption: date_to_test is less-than/before
+    date_today. Assumption: the date is a valid date.
+    :param date_to_test: a string in the form of "YYYY-MM-DD" representing a date
+    :param date_today: a string in the form of "YYYY-MM-DD" representing a date
+    :return: True if the date_to_test is within or greater than 2 years. False otherwise
+    """
+    year_test = int(date_to_test[:4])
+    month_test = int(date_to_test[5:7])
+    day_test = int(date_to_test[8:])
+
+    year_today = int(date_today[:4])
+    month_today = int(date_today[5:7])
+    day_today = int(date_today[8:])
+
+    if year_test < (year_today - 2):
+        date_within_two_years = False
+    elif year_test > (year_today - 2):
+        date_within_two_years = True
+    elif month_test < month_today:
+        date_within_two_years = False
+    elif month_test > month_today:
+        date_within_two_years = True
+    elif day_test < day_today:
+        date_within_two_years = False
+    else:
+        date_within_two_years = True
+    return date_within_two_years
+
+def test_date_within_two_years():
+    assert date_within_two_years("2014-03-04","2015-12-16")
+    assert date_within_two_years("2013-03-04","2015-12-16") == False
+    assert date_within_two_years("2012-03-04","2015-12-16") == False
+    assert date_within_two_years("2011-03-04","2015-12-16") == False
+    assert date_within_two_years("2013-11-17","2015-11-16")
+    assert date_within_two_years("2013-11-16","2015-11-16")
+    assert date_within_two_years("2013-11-15","2015-11-16") == False
+    assert date_within_two_years("2013-10-17","2015-11-16") == False
+
+def valid_passport(passport_number):
+    """
+    This function checks to see if a passport number is valid, as defined by having five groups of
     alphanumeric characters (case-insensitive), separated by dashes.
 
-    :param passport_or_visa_number: string that represents a passport or visa number
+    :param passport_number: string that represents a passport or visa number
     :return: True, if the string conforms to the regular expression, False otherwise.
     """
     valid_regex = re.compile(r'^([\dA-Za-z]{5}-){4}[\dA-Za-z]{5}$')
-    valid_regex_match = valid_regex.search(passport_or_visa_number)
+    valid_regex_match = valid_regex.search(passport_number)
     valid = True
     if valid_regex_match is None:
         valid = False
     return valid
 
-def test_valid_passport_or_visa():
+def test_valid_passport():
     s1 = "6P294-42HR2-95PSF-93NFF-2TEWF"
-    assert valid_passport_or_visa(s1)
+    assert valid_passport(s1)
     s2 = "TJq2R-25stx-Fyc52-02rm0-420DS"
-    assert valid_passport_or_visa(s2)
+    assert valid_passport(s2)
     s3 = "33T0R-8T3T2-W_C77-243GE-42O_D"
-    assert valid_passport_or_visa(s3) == False
+    assert valid_passport(s3) == False
     s4 = "T2EW5-WT255-019RW-2RWS4-42FFX-TNX2R"
-    assert valid_passport_or_visa(s4) == False
+    assert valid_passport(s4) == False
 
 def valid_date(date):
     """
@@ -315,10 +480,10 @@ def valid_file_contents(file_contents):
                 if not valid_location_field(person[item]):
                     valid_file = False
             elif item is "passport":
-                if not valid_passport_or_visa(person[item]):
+                if not valid_passport(person[item]):
                     valid_file = False
             elif item is "visa":
-                if not valid_passport_or_visa(person[item]['code']) or not valid_date(person[item]['date']):
+                if not valid_visa(person[item]['code']) or not valid_date(person[item]['date']):
                     valid_file = False
             elif item is "entry_reason":
                 if person[item] not in REASON_FOR_ENTRY:
